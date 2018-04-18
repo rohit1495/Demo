4497dad57afbefb077d1d88a01369a5eb95639a4
rohit1495 <rohit.singh@nestaway.com>
2018-04-18 19:26:38 +0530

    new

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'e3d19884440e91623b638f1a9823c1a6a567265673be60ba2aa0e44d12f11b7ce03f6998ebf696d9a6895ea925824625f3565b9fdcf85c922b91f04bdae1b802'
